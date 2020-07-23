a743a41b81396e5df2e6754b9e364bc4f2a3ffd3
danielatvalpo <45440753+danielatvalpo@users.noreply.github.com>
2020-07-22 19:28:49 -0500

    Try adding journal entries

---
+++ models/journalentry.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const journalentrySchema = new Schema({
+  prompt: { type: String, required: true },
+  response: { type: String, required: true },
+  date: { type: Date, required: true },
+}, {
+  timestamps: true,
+});
+
+const JournalEntry = mongoose.model('JournalEntry', journalentrySchema);
+
+module.exports = JournalEntry;
