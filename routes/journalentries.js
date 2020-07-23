0821ff31653c9b91f89cb7ea8f425cba52988b5d
danielatvalpo <45440753+danielatvalpo@users.noreply.github.com>
2020-07-22 20:14:16 -0500

    Fix attempt

--- routes/journalentries.js
+++ routes/journalentries.js
@@ -1,24 +1,47 @@
 const router = require('express').Router();
-
 let Entry = require('../models/journalentry.model');
 
-router.route('/').get((req,res) => {
-	Entry.find()
-    .then(entries => res.json(users))
+router.route('/').get((req, res) => {
+  Entry.find()
+    .then(entries => res.json(entries))
     .catch(err => res.status(400).json('Error: ' + err));
 });
-
 
 router.route('/add').post((req, res) => {
   const prompt = req.body.prompt;
   const response = req.body.response;
 
- const newEntry = new Entry({
-	 prompt,
-	 response,
- });
- 
- newEntry.save()
- .then(() => res.json('Entry added!'))
- .catch(err => res.status(400).json('Error: ' + err));
+  const newEntry = new Entry({
+    prompt,
+    response,
+  });
+
+  newEntry.save()
+  .then(() => res.json('Entry added!'))
+  .catch(err => res.status(400).json('Error: ' + err));
 });
+
+router.route('/:id').get((req, res) => {
+  Entry.findById(req.params.id)
+    .then(entry => res.json(entry))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+router.route('/:id').delete((req, res) => {
+  Entry.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Entry deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+router.route('/update/:id').post((req, res) => {
+  Entry.findById(req.params.id)
+    .then(entry => {
+      entry.prompt = req.body.prompt;
+      entry.response= req.body.response;
+      
+      entry.save()
+        .then(() => res.json('Entry updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
